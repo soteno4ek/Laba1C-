6e9500e3c4a4a8719a073ebf4e3ed4f97acd2e65
soteno4ek <eshnire120@gmail.com>
2026-09-25 13:51:37 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main()
     {
-        Methods p = new Methods();
+        Methods m = new Methods();
 
         Console.WriteLine("ЛАБОРАТОРНАЯ РАБОТА №1");
         Console.WriteLine();
@@ -57,7 +57,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Дробная часть числа: {p.fraction(x1)}");
+                    Console.WriteLine($"Дробная часть числа: {m.fraction(x1)}");
                     break;
 
                 case 2:
@@ -72,7 +72,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.charToNum(x2)}");
+                    Console.WriteLine($"Результат: {m.charToNum(x2)}");
                     break;
 
                 case 3:
@@ -82,7 +82,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.is2Digits(x3)}");
+                    Console.WriteLine($"Результат: {m.is2Digits(x3)}");
                     break;
 
                 case 4:
@@ -104,7 +104,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.isInRange(x4a, x4b, x4num)}");
+                    Console.WriteLine($"Результат: {m.isInRange(x4a, x4b, x4num)}");
                     break;
 
                 case 5:
@@ -126,7 +126,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.isEqual(x5a, x5b, x5c)}");
+                    Console.WriteLine($"Результат: {m.isEqual(x5a, x5b, x5c)}");
                     break;
 
                 case 6:
@@ -136,7 +136,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Модуль числа {x6}: {p.abs(x6)}");
+                    Console.WriteLine($"Модуль числа {x6}: {m.abs(x6)}");
                     break;
 
                 case 7:
@@ -146,7 +146,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.is35(x7)}");
+                    Console.WriteLine($"Результат: {m.is35(x7)}");
                     break;
 
                 case 8:
@@ -168,7 +168,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Максимальное: {p.max3(x8x, x8y, x8z)}");
+                    Console.WriteLine($"Максимальное: {m.max3(x8x, x8y, x8z)}");
                     break;
 
                 case 9:
@@ -184,7 +184,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.sum2(x9x, x9y)}");
+                    Console.WriteLine($"Результат: {m.sum2(x9x, x9y)}");
                     break;
 
                 case 10:
@@ -194,7 +194,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.day(x10)}");
+                    Console.WriteLine($"Результат: {m.day(x10)}");
                     break;
 
                 case 11:
@@ -204,7 +204,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.listNums(x11)}");
+                    Console.WriteLine($"Результат: {m.listNums(x11)}");
                     break;
 
                 case 12:
@@ -214,7 +214,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.chet(x12)}");
+                    Console.WriteLine($"Результат: {m.chet(x12)}");
                     break;
 
                 case 13:
@@ -224,7 +224,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.numLen(x13)}");
+                    Console.WriteLine($"Результат: {m.numLen(x13)}");
                     break;
 
                 case 14:
@@ -235,7 +235,7 @@ class Program
                         break;
                     }
                     Console.WriteLine("Результат:");
-                    p.square(x14);
+                    m.square(x14);
                     break;
 
                 case 15:
@@ -246,11 +246,11 @@ class Program
                         break;
                     }
                     Console.WriteLine("Результат:");
-                    p.rightTriangle(x15);
+                    m.rightTriangle(x15);
                     break;
 
                 case 16:
-                    int[] arr16 = p.ReadArray();
+                    int[] arr16 = m.ReadArray();
                     if (arr16 == null)
                     {
                         break;
@@ -261,25 +261,25 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.findFirst(arr16, x16)}");
+                    Console.WriteLine($"Результат: {m.findFirst(arr16, x16)}");
                     break;
 
                 case 17:
-                    int[] arr17 = p.ReadArray();
+                    int[] arr17 = m.ReadArray();
                     if (arr17 == null)
                     {
                         break;
                     }
-                    Console.WriteLine($"Результат: {p.maxAbs(arr17)}");
+                    Console.WriteLine($"Результат: {m.maxAbs(arr17)}");
                     break;
 
                 case 18:
-                    int[] arr18a = p.ReadArray();
+                    int[] arr18a = m.ReadArray();
                     if (arr18a == null)
                     {
                         break;
                     }
-                    int[] arr18b = p.ReadArray();
+                    int[] arr18b = m.ReadArray();
                     if (arr18b == null)
                     {
                         break;
@@ -290,20 +290,20 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {string.Join(", ", p.add(arr18a, arr18b, pos18))}");
+                    Console.WriteLine($"Результат: {string.Join(", ", m.add(arr18a, arr18b, pos18))}");
                     break;
 
                 case 19:
-                    int[] arr19 = p.ReadArray();
+                    int[] arr19 = m.ReadArray();
                     if (arr19 == null)
                     {
                         break;
                     }
-                    Console.WriteLine($"Результат: {string.Join(", ", p.reverseBack(arr19))}");
+                    Console.WriteLine($"Результат: {string.Join(", ", m.reverseBack(arr19))}");
                     break;
 
                 case 20:
-                    int[] arr20 = p.ReadArray();
+                    int[] arr20 = m.ReadArray();
                     if (arr20 == null)
                     {
                         break;
@@ -314,7 +314,7 @@ class Program
                         Console.WriteLine("Некорректный ввод");
                         break;
                     }
-                    Console.WriteLine($"Результат: {string.Join(", ", p.findAll(arr20, x20))}");
+                    Console.WriteLine($"Результат: {string.Join(", ", m.findAll(arr20, x20))}");
                     break;
 
                 case 0:
@@ -327,270 +327,5 @@ class Program
                     break;
             }
         } while (running);
-    }
-}
-
-class Methods
-{
-        public double fraction(double x)
-    {
-        return x - (int)x;
-    }
-
-    public int charToNum(char x)
-    {
-        return x - '0';
-    }
-
-    public bool is2Digits(int x)
-    {
-        return x >= 10 && x <= 99;
-    }
-
-    public bool isInRange(int a, int b, int num)
-    {
-        if (a <= b)
-        {
-            return a <= num && num <= b;
-        }
-        else
-        {
-            return b <= num && num <= a;
-        }
-    }
-
-    public bool isEqual(int a, int b, int c)
-    {
-        return a == b && b == c;
-    }
-
-    public int abs(int x)
-    {
-        if (x < 0)
-        {
-            return -x;
-        }
-        else
-        {
-            return x;
-        }
-    }
-
-    public bool is35(int x)
-    {
-        return (x % 3 == 0) != (x % 5 == 0);
-    }
-
-    public int max3(int x, int y, int z)
-    {
-        int max = x;
-        if (max < y)
-        {
-            max = y;
-        }
-        if (max < z)
-        {
-            max = z;
-        }
-        return max;
-    }
-
-    public int sum2(int x, int y)
-    {
-        int sum = x + y;
-        if (sum >= 10 && sum <= 19)
-        {
-            return 20;
-        }
-        return sum;
-    }
-
-    public string day(int x)
-    {
-        switch (x)
-        {
-            case 1:
-                return "понедельник";
-
-            case 2:
-                return "вторник";
-
-            case 3:
-                return "среда";
-
-            case 4:
-                return "четверг";
-
-            case 5:
-                return "пятница";
-
-            case 6:
-                return "суббота";
-
-            case 7:
-                return "воскресенье";
-
-            default:
-                return "это не день недели";
-        }
-    }
-
-    public string listNums(int x)
-    {
-        string s = "0";
-        for (int i = 1; i <= x; i++)
-        {
-            s += " " + i.ToString();
-        }
-        return s;
-    }
-
-    public string chet(int x)
-    {
-        string s = "0";
-        for (int i = 2; i <= x; i += 2)
-        {
-            s += " " + i.ToString();
-        }
-        return s;
-    }
-
-    public int numLen(long x)
-    {
-        int count = 0;
-        long temp = x;
-        while (temp != 0)
-        {
-            count++;
-            temp /= 10;
-        }
-        return count;
-    }
-
-    public void square(int x)
-    {
-        for (int i = 1; i <= x; i++)
-        {
-            for (int j = 1; j <= x; j++)
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine();
-        }
-    }
-
-    public void rightTriangle(int x)
-    {
-        for (int i = 1; i <= x; i++)
-        {
-            for (int j = 1; j <= x - i; j++)
-            {
-                Console.Write(" ");
-            }
-
-            for (int j = 1; j <= i; j++)
-            {
-                Console.Write("*");
-            }
-            Console.WriteLine();
-        }
-    }
-
-    public int[] ReadArray()
-    {
-        Console.Write("Введите размер массива: ");
-        if (!int.TryParse(Console.ReadLine(), out int n))
-        {
-            return null;
-        }
-
-        int[] arr = new int[n];
-
-        for (int i = 0; i < n; i++)
-        {
-            Console.Write($"Введите {i + 1} элемент: ");
-            if (!int.TryParse(Console.ReadLine(), out arr[i]))
-            {
-                return null;
-            }
-        }
-        return arr;
-    }
-
-    public int findFirst(int[] arr, int x)
-    {
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (arr[i] == x)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public int maxAbs(int[] arr)
-    {
-        int max = arr[0];
-        for (int i = 1; i < arr.Length; i++)
-        {
-            if (Math.Abs(arr[i]) > Math.Abs(max))
-            {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
-    public int[] add(int[] arr, int[] ins, int pos)
-    {
-        int[] res = new int[arr.Length + ins.Length];
-        for (int i = 0; i < pos; i++)
-        {
-            res[i] = arr[i];
-        }
-        for (int i = 0; i < ins.Length; i++)
-        {
-            res[pos + i] = ins[i];
-        }
-        for (int i = pos; i < arr.Length; i++)
-        {
-            res[i + ins.Length] = arr[i];
-        }
-
-        return res;
-    }
-
-    public int[] reverseBack(int[] arr)
-    {
-        int[] res = new int[arr.Length];
-        for (int i = arr.Length - 1; i >= 0; i--)
-        {
-            res[arr.Length - i - 1] = arr[i];
-        }
-        return res;
-    }
-
-    public int[] findAll(int[] arr, int x)
-    {
-        int count = 0;
-        for (int i = 0; i < arr.Length; i++)
-        {
-            if (arr[i] == x)
-            {
-                count++;
-            }
-        }
-        int idx = 0;
-        int[] res = new int[count];
-        for (int j = 0; j < arr.Length; j++)
-        {
-            if (arr[j] == x)
-            {
-                res[idx] = j;
-                idx++;
-            }
-        }
-        return res;
     }
 }
